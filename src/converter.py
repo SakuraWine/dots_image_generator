b302b502d0329e46230b3dbc391612e0daca160c
Sakura Wine <sakura01n@gmail.com>
2023-09-09 13:29:54 +0900

    iroiro update

--- src/converter.py
+++ src/converter.py
@@ -1,5 +1,9 @@
 import cv2
 import cv2.typing
+import numpy as np
+import numpy._typing
+from typing import List
+import glob
 
 
 class DotsImageGenerator(object):
@@ -15,12 +19,31 @@ class DotsImageGenerator(object):
             image (cv2.typing.MatLike): source
             level (int): level
         """
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # 元画像のサイズを覚えとく
+        height, width, channels = image.shape[:3]
+        # 輝度値を見るための形式へ変換
+        image_resized = cv2.resize(image, None, None, 1.0, 0.5)
+        image_gray = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
         source = self.resize_image(image=image_gray, level=level)
+        # 輝度値を点字に変換
+        dots_lines: List[str] = []
         for column in source:
+            dots_list: List[str] = []
             for row in column:
-                print(self.get_dots(row), end="")
-            print()
+                dots = self.get_dots(row)
+                dots_list.append(dots)
+            # 一文字ずつの文字の配列を一続きの文字列へ変換する
+            dots_str = "".join(dots_list)
+            dots_lines.append(dots_str)
+        # 出力
+        for line in dots_lines:
+            print(line)
+        blank_image = np.zeros((height, width, 1))
+        self.draw_dots(blank_image, dots_lines)
+
+    def draw_dots(self, image: numpy._typing.NDArray, dots_lines: List[str]) -> bool:
+        # TODO: drawing dots to blank image is not implemented.
+        return True
 
     def get_dots(self, brightness: int) -> str:
         """convert brightness to dots
@@ -53,15 +76,15 @@ class DotsImageGenerator(object):
             cv2.typing.MatLike: resized image
         """
         image_rescaled = cv2.resize(image, None, None, 1.0, 0.7)
-        if level == 5:
+        if level == 1:
             image_resized = cv2.resize(image_rescaled, None, None, 0.15, 0.15)
-        elif level == 4:
+        elif level == 2:
             image_resized = cv2.resize(image_rescaled, None, None, 0.25, 0.25)
         elif level == 3:
             image_resized = cv2.resize(image_rescaled, None, None, 0.5, 0.5)
-        elif level == 2:
+        elif level == 4:
             image_resized = cv2.resize(image_rescaled, None, None, 0.75, 0.75)
-        elif level == 1:
+        elif level == 5:
             image_resized = image_rescaled
         else:
             print("Invalid level.")
@@ -77,11 +100,11 @@ class DotsImageGenerator(object):
         """
         # read
         image = cv2.imread(source_path, cv2.IMREAD_COLOR)
-        # resize
-        image_resized = cv2.resize(image, None, None, 1.0, 0.5)
         # to grayscale
-        self.to_dots(image=image_resized, level=level)
+        self.to_dots(image=image, level=level)
 
 
 generator = DotsImageGenerator()
-generator.generate("/home/sakura/workspace/dots_image_generator/data/sample_miyako.png", 5)
+sources = glob.glob("./data/*.png")
+for source in sources:
+    generator.generate(source, 1)
